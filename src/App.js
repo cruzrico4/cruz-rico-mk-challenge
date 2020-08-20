44ee483350f12339c9ab8ed01e67430eb611fbc0
Cruz <jcrico@ucsd.edu>
2020-08-19 21:30:27 -0700

    getting ready for first deploy

--- src/App.js
+++ src/App.js
@@ -1,26 +1,38 @@
 import React from 'react';
 import logo from './logo.svg';
-import './App.css';
+import Box from '@material-ui/core/Box'
+import Spacing from '@material-ui/system'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import LoginWindow from './Login/LoginWindow.js'
+import { createMuiTheme, Typography } from '@material-ui/core'
+import {sizing} from '@material-ui/system'
+import { ThemeProvider } from '@material-ui/core';
+
+//Padding between elements
+const theme = createMuiTheme({
+  spacing: 8,
+  shape: {
+    borderRadius: 0,
+  },
+});
+
+
+export class App extends React.Component{
+
+  render() {
+    return (
+    <ThemeProvider theme={theme}>
+      
+      <Box 
+        display="flex"
+        justifyContent="center"
+        m="5%">
+        <LoginWindow/>
+      </Box>
+    </ThemeProvider>
+    
+    );
+  }
 }
 
 export default App;
